2c828eb58bb234edda6bb454dc25f97b190c08a7
fanner7 <raogy@vip.qq.com>
2026-07-12 10:35:02 +0800

    全局启用页面转发功能
    在 app.js 中重写 Page 构造器，为所有未自定义 onShareAppMessage
    的页面注入默认分享回调，点亮所有页面右上角分享按钮

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // app.js
+// 全局注入 onShareAppMessage，点亮所有页面的转发按钮
+const _Page = Page;
+Page = function (opts) {
+  if (!opts.onShareAppMessage) {
+    opts.onShareAppMessage = function () {
+      return {
+        title: '冒险背包',
+        path: '/' + (this.route || 'pages/index/index'),
+      };
+    };
+  }
+  return _Page(opts);
+};
+
 App({
   onLaunch() {
     // 展示本地存储能力
